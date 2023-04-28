0585fa3a5277d11413816412432935a45d853ab7
aionalaw <71141380+aionalaw@users.noreply.github.com>
2023-04-28 17:23:01 -0400

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-conn = sqlite3.connect('database.db')
-print("opened database successfully")
-
-conn.exceute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print("Table created successfully")
-
-conn.close()
